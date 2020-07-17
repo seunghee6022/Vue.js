36257b41ecce3e2b684d973ee5c6986a50b72386
SH <seunghee6022@gmail.com>
2020-07-17 22:52:44 +0900

    npm install axios

---
+++ 0602 Vue Youtube Clone Coding/vue-youtube-browser/src/components/SearchBar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+      SearchBar
+      <input type="text" @keypress.enter="submit">
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'SearchBar',
+
+    methods: {
+        submit(event) {
+            console.log(event.target.value)
+            this.$emit('event-submit',event.target.value)
+        }
+    }
+
+    
+}
+</script>
+
+<style>
+
+</style>
---
+++ 0602 Vue Youtube Clone Coding/vue-youtube-browser/src/views/Main.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+      
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Youtube.vue'
+}
+</script>
+
+<style>
+
+</style>
